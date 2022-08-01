89467a5600c92d69e96bd98121771e100f44b68c
Eduardo Franz <eduardo.franz77@gmail.com>
2022-08-01 12:47:08 -0300

    add navbar e footer Login v.2

--- src/static/navbar/Navbar.tsx
+++ src/static/navbar/Navbar.tsx
@@ -1,12 +1,46 @@
 import React from 'react'
-// import { Typography, Grid, Button } from '@material-ui/core'
-// import Box from '@mui/material'
+import {AppBar, Toolbar, Typography} from '@material-ui/core';
+import {Box} from '@mui/material'
+import {Link} from 'react-router-dom';
 import './Navbar.css'
+
 
 function Navbar() {
   return (
-    <>
-    </>
-)
+      <>
+          <AppBar position="static" color='secondary'>
+              <Toolbar className="background" variant="dense">
+                  <Box className='cursor'>
+                      <Typography variant="h5" color="inherit">
+                          TODA DONA
+                      </Typography>
+                  </Box>
+
+                  <Box display="flex" justifyContent="start">
+                      <Box mx={1} className='cursor'>
+                          <Typography variant="h6" color="inherit">
+                              Home
+                          </Typography>
+                      </Box>
+                      <Box mx={1} className='cursor'>
+                          <Typography variant="h6" color="inherit">
+                              Contato
+                          </Typography>
+                      </Box>
+                      <Link to='/login' className='text-decorator-none'>
+                          <Box mx={1} className='cursor'>
+                              <Typography variant="h6" color="inherit">
+                                  Logout
+                              </Typography>
+                          </Box>
+                      </Link>
+                      
+                  </Box>
+
+              </Toolbar>
+          </AppBar>
+      </>
+  )
 }
+
 export default Navbar;
