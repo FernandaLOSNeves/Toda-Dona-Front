b3152f593dd51f7d2865246d37c926c32514f919
Bruna Franchi <bfzdev@gmail.com>
2022-08-20 20:11:38 -0300

    modificacoes do footer

--- src/components/produtos/listaprodutos/ListaProdutos.tsx
+++ src/components/produtos/listaprodutos/ListaProdutos.tsx
@@ -1,24 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { busca } from '../../../services/Service'
-import { Box } from '@mui/material';
-import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
-import './ListaProdutos.css';
+import { Box } from '@mui/material'
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+  Grid
+} from '@material-ui/core'
+import './ListaProdutos.css'
 import { useNavigate } from 'react-router-dom'
-import Produto from '../../../models/Produto';
-import { toast } from 'react-toastify';
-import { useSelector } from 'react-redux';
-import { TokenState } from '../../../store/tokens/tokensReducer';
-
-
+import Produto from '../../../models/Produto'
+import { toast } from 'react-toastify'
+import { useSelector } from 'react-redux'
+import { TokenState } from '../../../store/tokens/tokensReducer'
 
 
 function ListaProdutos() {
   const [produtos, setProdutos] = useState<Produto[]>([])
-  let navigate = useNavigate();
-  const token = useSelector<TokenState, TokenState["tokens"]>(
-    (state) => state.tokens
-  );
+  let navigate = useNavigate()
+  const token = useSelector<TokenState, TokenState['tokens']>(
+    state => state.tokens
+  )
 
   //   useEffect(() => {
   //     if (token == "") {
@@ -37,74 +42,89 @@ function ListaProdutos() {
   // }, [token]);
 
   async function getProduto() {
-    await busca("/produto", setProdutos, {
+    await busca('/produto', setProdutos, {
       headers: {
-        'Authorization': token
+        Authorization: token
       }
     })
   }
 
   useEffect(() => {
     getProduto()
-
   }, [produtos.length])
 
   return (
     <>
-      {
-        produtos.map(produto => (
+      {produtos.map(produto => (
+        
+            <Box m={6}  className="cardtamanho">
+              <Card variant="outlined" >
+                <CardContent>
+                  <Typography color="textSecondary" gutterBottom>
+                    Produtos
+                  </Typography>
+                  <Typography variant="h5" component="h2">
+                    {produto.nome_produto}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {produto.descricao_produto}
+                  </Typography>
+                  <Box>
+                    <img
+                      src={produto.fotoProduto}
+                      alt="foto produto"
+                      className="imagem-produto"
+                    />
+                  </Box>
 
-          <Box m={6} className='cardtamanho'  >
-            <Card  variant="outlined">
-              <CardContent  >
-                <Typography color="textSecondary" gutterBottom>
-                  Produtos
-                </Typography>
-                <Typography variant="h5" component="h2">
-                  {produto.nome_produto}
-                </Typography>
-                <Typography variant="body2" component="p">
-                  {produto.descricao_produto}
-                </Typography>
-                <Box>
-                  <img src={produto.fotoProduto} alt="foto produto" className="imagem-produto" />
-                </Box>
-
-                <Typography variant="body2" component="p">
-                  {produto.categorias?.categoria}
-                </Typography>
-                <Typography variant="body2" component="p">R$
-                  {produto.valor_produto}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Box display="flex" justifyContent="center" mb={1.5}>
-
-                  <Link to={`/formularioProduto/${produto.id}`} className="text-decorator-none" >
-                    <Box mx={1}>
-                      <Button variant="contained" className="marginLeft" size='small' color="primary" >
-                        atualizar
-                      </Button>
-                    </Box>
-                  </Link>
-                  <Link to={`/deletarProduto/${produto.id}`} className="text-decorator-none">
-                    <Box mx={1}>
-                      <Button variant="contained" size='small' color="secondary">
-                        deletar
-                      </Button>
-                    </Box>
-                  </Link>
-
-                </Box>
-              </CardActions>
-            </Card>
-          </Box>
-
-        ))
-      }
+                  <Typography variant="body2" component="p">
+                    {produto.categorias?.categoria}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    R$
+                    {produto.valor_produto}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Box display="flex" justifyContent="center" mb={1.5}>
+                    <Link
+                      to={`/formularioProduto/${produto.id}`}
+                      className="text-decorator-none"
+                    >
+                      <Box mx={1}>
+                        <Button
+                          variant="contained"
+                          className="marginLeft"
+                          size="small"
+                          color="primary"
+                        >
+                          atualizar
+                        </Button>
+                      </Box>
+                    </Link>
+                    <Link
+                      to={`/deletarProduto/${produto.id}`}
+                      className="text-decorator-none"
+                    >
+                      <Box mx={1}>
+                        <Button
+                          variant="contained"
+                          size="small"
+                          color="secondary"
+                        >
+                          deletar
+                        </Button>
+                      </Box>
+                    </Link>
+                  </Box>
+                </CardActions>
+              </Card>
+            </Box>
+        
+         
+      ))}
     </>
   )
 }
 
-export default ListaProdutos;
-
+export default ListaProdutos
--- src/components/static/footer/Footer.tsx
+++ src/components/static/footer/Footer.tsx
@@ -1,40 +1,136 @@
 import React from 'react'
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import { Typography, Grid, Button } from '@material-ui/core'
-import {Box} from '@mui/material'
+import InstagramIcon from '@material-ui/icons/Instagram'
+import FacebookIcon from '@material-ui/icons/Facebook'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import { Typography, Grid, Button, TextField } from '@material-ui/core'
+import { Box } from '@mui/material'
 import './Footer.css'
 
 function Footer() {
   return (
     <>
-                <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box className='box1'>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
-                                <FacebookIcon className='redes' />
-                            </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <InstagramIcon className='redes'/>
-                            </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <LinkedInIcon className='redes' />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box className='box2'>
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom className='textos'>© 2022 Copyright: TodaDona Inc.</Typography>
-                        </Box>
-                        <Box>                            
-                                <Typography variant="subtitle2" gutterBottom className='textos' align="center">https://todadona.com.br</Typography>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid alignItems="center" item xs={2} className="background-2">
+          <Box paddingX={10} paddingY={2}>
+            <Typography className="fonte2" gutterBottom color="textPrimary">
+              Site
+            </Typography>
+            <Typography className="fonte1" gutterBottom color="textPrimary">
+              Produtos
+            </Typography>
+            <Typography className="fonte1" gutterBottom color="textPrimary">
+              Serviços
+            </Typography>
+            <Typography className="fonte1" gutterBottom color="textPrimary">
+              Categorias
+            </Typography>
+            <Typography className="fonte1" gutterBottom color="textPrimary">
+              Vendedora
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={4} className="background-2">
+          <Box paddingX={5} paddingY={5}>
+            <Typography className="fonte2" gutterBottom color="textPrimary">
+              Assine a nossa Newsletter
+            </Typography>
+
+            <Typography className="fonte1" gutterBottom color="textPrimary">
+              Para ficar toda informada sobre novidades
+            </Typography>
+            <TextField
+            className='textfield'
+              id="email"
+              label="E-mail"
+              name="email"
+              margin="normal"
+              type="email"
+              fullWidth
+            ></TextField>
+          </Box>
+        </Grid>
+        <Grid alignItems="center" item xs={3} className="background-2">
+          <Box paddingX={5} paddingY={2}>
+            <Typography className="fonte2" gutterBottom color="textPrimary">
+              Sobre nós
+            </Typography>
+            <Typography className="fonte1" gutterBottom color="textPrimary">
+              Equipe
+            </Typography>
+            <Typography className="fonte1" gutterBottom color="textPrimary">
+              O modelo TodaDona
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid alignItems="center" item xs={3} className="background-2">
+          <Box paddingX={5} paddingY={2}>
+            <Typography className="fonte2" gutterBottom color="textPrimary">
+              Contato
+            </Typography>
+            <Typography className="fonte1" gutterBottom color="textPrimary">
+              Investidores
+            </Typography>
+            <Typography className="fonte1" gutterBottom color="textPrimary">
+              Imprensa
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid alignItems="center" item xs={12}>
+          <Box className="box1">
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <a
+                href="https://www.facebook.com/generationbrasil"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FacebookIcon className="redes" />
+              </a>
+              <a
+                href="https://www.instagram.com/generationbrasil/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <InstagramIcon className="redes" />
+              </a>
+              <a
+                href="https://www.linkedin.com/school/generationbrasil/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LinkedInIcon className="redes" />
+              </a>
+            </Box>
+          </Box>
+          <Box className="box2">
+            <Box paddingTop={1}>
+              <Typography
+                variant="subtitle2"
+                align="center"
+                gutterBottom
+                className="textos"
+              >
+                © 2022 Copyright: TodaDona Inc.
+              </Typography>
+            </Box>
+            <Box>
+              <Typography
+                variant="subtitle2"
+                gutterBottom
+                className="textos"
+                align="center"
+              >
+                https://todadona.com.br
+              </Typography>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
     </>
-)
+  )
 }
-export default Footer;
+export default Footer
