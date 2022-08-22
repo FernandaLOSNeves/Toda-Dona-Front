7978f6ed3348b8f60aa534ecdc25d06dd3ddf78d
Bruna Franchi <bfzdev@gmail.com>
2022-08-22 12:05:12 -0300

    arrumando responsividade de fale conosco e contato

--- src/paginas/contato/Contato.tsx
+++ src/paginas/contato/Contato.tsx
@@ -1,49 +1,102 @@
-import { Grid } from '@material-ui/core';
 import React from 'react'
-// import { Typography, Grid, Button } from '@material-ui/core'
+import { Box } from '@mui/material'
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+  Grid
+} from '@material-ui/core'
 // import Box from '@mui/material'
 import './Contato.css'
 
 function Contato() {
   return (
     <>
-      <Grid container className='padding'>
-        <Grid item xs={12} className='textoConta'>
-          <h1 className='quem'>Quem somos nós?</h1>
-          <h2 className='branco2'>Nós somos todas as mulheres com vontade de empreender em um só lugar.<br />Um espaço só para elas.</h2>
+      <Grid container className="padding">
+        <Grid item xs={12} className="textoConta">
+          <Typography className="quem">Quem somos nós?</Typography>
+          <Typography className="branco2">
+            Nós somos todas as mulheres com vontade de empreender em um só
+            lugar.
+          </Typography>
+          <Typography className="branco2">Um espaço só para elas.</Typography>
         </Grid>
       </Grid>
 
-      <Grid xs={12} className='quebra'>
-        <h3 className='grand'>A TodaDona nasceu com a missão de ser um espaço exclusivo para mulheres realizarem compra e vendas de produtos e serviços com 0% de taxas de serviço cobradas pelo site. Para ajudar toda e qualquer mulher no desenvolvimento do seu negócio.</h3>
+      <Grid xs={12} className="quebra">
+        <Typography className="grand">
+          A TodaDona nasceu com a missão de ser um espaço exclusivo para
+          mulheres realizarem compra e vendas de produtos e serviços com 0% de
+          taxas de serviço cobradas pelo site. Para ajudar toda e qualquer
+          mulher no desenvolvimento do seu negócio.
+        </Typography>
       </Grid>
-      <Grid container className='contain'>
-        <Grid item xs={3} className='card'>
-          <h2 className='cardT'>Nossa Missão</h2>
-          <h3>Nossa missão é incluir as mulheres no mercado de vendas online por meio da facilitação da venda de seus produtos ou serviços na internet.</h3>
-        </Grid>
-        <Grid item xs={3} className='card'>
-          <h2 className='cardT'>Nossa Visão</h2>
-          <h3>Criar um espaço virtual de vendas que permita que mulheres empreendedoras alcancem sua liberdade financeira.</h3>
-        </Grid>
-        <Grid xs={3} className='card2'>
-          <h2 className='cardT'>Nossos Valores</h2>
-          <h3>Respeito com todes</h3>
-          <h3>Empatia</h3>
-          <h3>Ética Profissional</h3>
-          <h3>Comunicação aberta</h3>
-        </Grid>
+
+      <Box m={6} className="cardsContato">
+        <Card className="cardInvid">
+          <CardContent>
+            <Typography className="typo1">Nossa Missão</Typography>
+            <Typography className="typo2">
+              Nossa missão é incluir as mulheres no mercado de vendas online por
+              meio da facilitação da venda de seus produtos ou serviços na
+              internet.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card className="cardInvid">
+          <CardContent>
+            <Typography className="typo1">Nossa Visão</Typography>
+
+            <Typography className="typo2">
+              Criar um espaço virtual de vendas que permita que mulheres
+              empreendedoras alcancem sua liberdade financeira.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card className="cardInvid">
+          <CardContent>
+            <Typography className="typo1"> Nossos Valores </Typography>
+            <Typography className="typo2">
+              Respeito com todes, empatia, ética profissional e comunicação
+              aberta.{' '}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+
+      <Grid xs={12} className="redir">
+        <Typography className="typo2">
+          {' '}
+          Quer saber mais informações sobre nós? Mande um email para{' '}
+          <a className="cru" href="mailto:todainformada@gmail.com">
+            todainformada@gmail.com
+          </a>{' '}
+        </Typography>
       </Grid>
-      <Grid xs={12} className='redir'>
-        <h3>Quer saber mais informações sobre nós? Mande um email para <a className='cru' href="mailto:todainformada@gmail.com">todainformada@gmail.com</a></h3>
+      <Grid xs={12} className="redir">
+        <Typography className="typo2">
+          {' '}
+          Gostaria de ser uma investidora? Escreva para:{' '}
+          <a className="cru" href="mailto:todadonainvestidores@gmail.com">
+            todadonainvestidores@gmail.com
+          </a>
+        </Typography>
       </Grid>
-      <Grid xs={12} className='redir'>
-        <h3>Gostaria de ser uma investidora? Escreva para: <a className='cru' href="mailto:todadonainvestidores@gmail.com">todadonainvestidores@gmail.com</a></h3>
-      </Grid>
-      <Grid xs={12} className='redir2'>
-        <h3>Quer vender conosco? <a className='cru' href="/cadastro">Cadastre-se</a> ou faça <a className='cru' href="/login">Login</a> para continuar.</h3>
+      <Grid xs={12} className="redir2">
+        <Typography className="typo2">
+          Quer vender conosco?{' '}
+          <a className="cru" href="/cadastro">
+            Cadastre-se
+          </a>{' '}
+          ou faça{' '}
+          <a className="cru" href="/login">
+            Login
+          </a>{' '}
+          para continuar.
+        </Typography>
       </Grid>
     </>
   )
 }
-export default Contato;
+export default Contato
--- src/paginas/faleConosco/FaleConosco.tsx
+++ src/paginas/faleConosco/FaleConosco.tsx
@@ -11,43 +11,37 @@ function FaleConosco() {
         <Grid container>
 
             <Grid item className='padding00' xs={12}>
-                <h1 className='titulo1'>Fale com a gente! <br/></h1>
-                <h2 className='titulo2'>Estamos aqui para te atender e te escutar.<br/><br/><br/></h2>
+                <Typography className='titulo1'> Fale com a gente! </Typography>
+                <Typography className = "titulo2">Estamos aqui para te atender e te escutar.</Typography>
             </Grid>
 
             <Grid item xs={6} alignItems='flex-end'>
                 <Box paddingX={10} paddingY={10}>
                     <form>
-                        <Typography variant='h3' gutterBottom color='textPrimary' className="textos2"> No que podemos te ajudar? </Typography>
+                        <Typography gutterBottom color='textPrimary' className="textos2"> Em que podemos te ajudar? </Typography>
 
                         <Typography className= "letras"> Seu nome: </Typography>
 
-                        <TextField label= "nome" id='nome' variant='outlined' name='nome' fullWidth></TextField>
+                        <TextField className='margin-bottom' label= "nome" id='nome' variant='outlined' name='nome' fullWidth></TextField>
 
-                        <br></br>
-                        <br></br>
+                        
 
                         <Typography className= "letras"> Seu e-mail: </Typography>
 
-                        <TextField id='email' label='e-mail:' variant='outlined' name='email' fullWidth></TextField>
+                        <TextField className='margin-bottom' id='email' label='e-mail:' variant='outlined' name='email' fullWidth></TextField>
 
-                        <br></br>
-                        <br></br>
+                        
 
                         <Typography className= "letras"> Assunto: </Typography>
 
-                        <TextField id='assunto' label='assunto' variant='outlined' name='assunto' fullWidth></TextField>
+                        <TextField className='margin-bottom' id='assunto' label='assunto' variant='outlined' name='assunto' fullWidth></TextField>
 
-                        <br></br>
-                        <br></br>
 
                         <Typography className= "letras"> Sua mensagem pra gente: </Typography>
 
-                        <TextField id='mensagem' label='mensagem' variant='outlined' name='mensagem' fullWidth></TextField>
+                        <TextField className='margin-bottom' id='mensagem' label='mensagem' variant='outlined' name='mensagem' fullWidth></TextField>
                         <Box marginTop={2} textAlign='center'>
 
-                        <br></br>
-                        <br></br>
 
                             <Link to='/' className='text-decorator-none2'>
                             <Button type='submit' variant='contained' className='btnCad margin-right'>
